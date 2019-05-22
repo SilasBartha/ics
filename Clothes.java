97f393515ce30b001bd41ce0c966e66114ff9e49
Silas Bartha <bartsila533@gmail.com>
2019-05-22 13:04:04 -0400

    Fixed Indentation For Colors and Clothes

--- Clothes.java
+++ Clothes.java
@@ -6,38 +6,38 @@ public class Clothes {
 	
 	public static void main(String[] args) {
 		String[] colors = {
-				"Red ",
-				"Vermillion ",
-				"Orange ",
-				"Goldenrod ",
-				"Yellow ",
-				"Chartreuse ",
-				"Green ",
-				"Aquamarine ",
-				"Blue ",
-				"Violet ",
-				"Purple "
-			};
-			
-			String[] clothes = {
-				"Hat, ",
-				"Glasses, ",
-				"Mask, ",
-				"Scarf, ",
-				"Shirt, ",
-				"Jacket, ",
-				"Gloves, ",
-				"Belt, ",
-				"Pants, ",
-				"Shoes"
-			};
-			
-			for(String col: colors) {
-				for(String item: clothes) {
-					System.out.print(col + item);
-				}
-				System.out.println();
+			"Red ",
+			"Vermillion ",
+			"Orange ",
+			"Goldenrod ",
+			"Yellow ",
+			"Chartreuse ",
+			"Green ",
+			"Aquamarine ",
+			"Blue ",
+			"Violet ",
+			"Purple "
+		};
+		
+		String[] clothes = {
+			"Hat, ",
+			"Glasses, ",
+			"Mask, ",
+			"Scarf, ",
+			"Shirt, ",
+			"Jacket, ",
+			"Gloves, ",
+			"Belt, ",
+			"Pants, ",
+			"Shoes"
+		};
+		
+		for(String col: colors) {
+			for(String item: clothes) {
+				System.out.print(col + item);
 			}
+			System.out.println();
+		}
 	}
 	
 }
